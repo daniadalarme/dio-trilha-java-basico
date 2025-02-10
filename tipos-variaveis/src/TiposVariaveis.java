12ffcb7e9b778fd156725e6a08102951079d566d
daniadalarme <daniadalarme@gmail.com>
2025-02-09 21:32:17 -0300

    primeiros estudos basicos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // tipos primitivos
+        // estudo de classes
+
+        String meuNome = "Daniela";
+        final int IDADE = 35; //valor que não pode ser alterado
+        String idadeCompleta = IDADE + "/anos";
+        int anoNascimento = 1989;
+        String mesNascimento = "Fevereiro";
+        int diaNascimento = 23;
+        double salario = 10597.89;
+
+        // Imprimindo as variáveis
+        System.out.println("Nome: " + meuNome);
+        System.out.println("Idade: " + idadeCompleta);
+        System.out.println("Mês: " + mesNascimento);
+        System.out.println("Dia: " + diaNascimento + "de" + anoNascimento);
+        System.out.println("Salario da sorte: R$ " + salario);
     }
 }
